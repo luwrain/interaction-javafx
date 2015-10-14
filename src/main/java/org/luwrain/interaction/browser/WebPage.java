33d009398bc855064966c4544dbf8e00c51fe9ab
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-10-15 01:33:32 +0600

    First cleaning of WebPage.java completed

--- src/main/java/org/luwrain/interaction/browser/WebPage.java
+++ src/main/java/org/luwrain/interaction/browser/WebPage.java
@@ -1,7 +1,7 @@
 
 package org.luwrain.interaction.browser;
 
-//import java.awt.Rectangle;
+import java.awt.Rectangle;
 import java.util.LinkedHashMap;
 import java.util.Vector;
 import java.util.concurrent.Callable;
@@ -42,7 +42,7 @@ public class WebPage implements Browser
 	//public JFXPanel jfx=new JFXPanel();
 
 	// used to save DOM structure with RescanDOM
-    static private class NodeInfo
+    static class NodeInfo
     {
 	org.w3c.dom.Node node;
 	Rectangle rect;
@@ -51,9 +51,9 @@ public class WebPage implements Browser
     }
 
     // list of all nodes in web page
-    final Vector<NodeInfo> dom=new Vector<NodeInfo>();
+    Vector<NodeInfo> dom=new Vector<NodeInfo>();
     // index map for fast get node position
-    final LinkedHashMap<org.w3c.dom.Node,Integer> domIdx=new LinkedHashMap<org.w3c.dom.Node, Integer>();
+    LinkedHashMap<org.w3c.dom.Node,Integer> domIdx=new LinkedHashMap<org.w3c.dom.Node, Integer>();
 
     HTMLDocument htmlDoc=null;
     DOMWindowImpl htmlWnd=null;
@@ -72,7 +72,7 @@ public class WebPage implements Browser
 	return null;
     }
 
-    WebPage(JavaFxInteraction interaction)
+    public WebPage(JavaFxInteraction interaction)
     {
 	wi = interaction;
     }
@@ -114,47 +114,33 @@ public class WebPage implements Browser
 							    }
 							}
 							events.onChangeState(newState);
-					}
-				});
-				webEngine.getLoadWorker().progressProperty().addListener(new ChangeListener<Number>()
-				{
+									      }
+									  });
+
+		    webEngine.getLoadWorker().progressProperty().addListener(new ChangeListener<Number>()
+									     {
 					@Override public void changed(ObservableValue<? extends Number> ov,Number o,final Number n)
 					{
-						//Log.debug("web","progress: from "+o+" to "+n);
-						//SwingUtilities.invokeLater(new Runnable() { @Override public void run()
-						//{
 							events.onProgress(n);
-						//}});
 					}
 				});
-				webEngine.setOnAlert(new EventHandler<WebEvent<String>>()
-				{
-					@Override public void handle(final WebEvent<String> event)
-					{
+
+		    webEngine.setOnAlert(new EventHandler<WebEvent<String>>() {
+			    @Override public void handle(final WebEvent<String> event)
+			    {
 						Log.debug("web","t:"+Thread.currentThread().getId()+" ALERT:"+event.getData());
-						//SwingUtilities.invokeLater(new Runnable() { @Override public void run()
-						//{
 							events.onAlert(event.getData());
-						//}});
 					}
 				});
-				webEngine.setPromptHandler(new Callback<PromptData,String>()
-				{
+
+				webEngine.setPromptHandler(new Callback<PromptData,String>() {
 					@Override public String call(final PromptData event)
 					{
-						Log.debug("web","t:"+Thread.currentThread().getId()+" PROMPT:"+event.getMessage()+"', default '"+event.getDefaultValue()+"'");
-						//FutureTask<String> query=new FutureTask<String>(new Callable<String>(){
-						//	@Override public String call() throws Exception
-						//	{
+					    Log.debug("web","t:"+Thread.currentThread().getId()+" PROMPT:"+event.getMessage()+"', default '"+event.getDefaultValue()+"'");
 								return events.onPrompt(event.getMessage(),event.getDefaultValue());
-						//	}
-						//});
-						//SwingUtilities.invokeLater(query);
-						// FIXME: make error handling better
-						//try {return query.get();} catch(InterruptedException|ExecutionException e)
-						//{e.printStackTrace();return null;}
 					}
 				});
+
 				webEngine.setConfirmHandler(new Callback<String,Boolean>()
 				{
 					@Override public Boolean call(String param)
@@ -163,197 +149,209 @@ public class WebPage implements Browser
 						return events.onConfirm(param);
 					}
 				});
+
 				webEngine.setOnError(new EventHandler<WebErrorEvent>()
 				{
 					@Override public void handle(final WebErrorEvent event)
 					{
 						Log.debug("web","thread:"+(Platform.isFxApplicationThread()?"javafx":"main")+"ERROR:"+event.getMessage());
-						//SwingUtilities.invokeLater(new Runnable() { @Override public void run()
-						//{
 							events.onError(event.getMessage());
-						//}});
 					}
 				});
 
 				webView.setVisible(false);
 				wi.addWebViewControl(webView);
-				
-				if(emptyList) wi.setCurPage(that);
+
+				if(emptyList) 
+wi.setCurPage(that);
 			}
 		});
-	}
-	// remove WebPage from WebEngineInteraction's webPages list and stop WebEngine work, prepare for destroy
-	@Override public void Remove()
+    }
+
+    // remove WebPage from WebEngineInteraction's webPages list and stop WebEngine work, prepare for destroy
+    @Override public void Remove()
+    {
+	int pos=wi.webPages.indexOf(this);
+	final boolean success=wi.webPages.remove(this);
+	if(!success) 
+	    Log.warning("web","Can't found WebPage to remove it from WebEngineInteraction");
+	setVisibility(false);
+	if(pos!=-1)
 	{
-		int pos=wi.webPages.indexOf(this);
-		boolean success=wi.webPages.remove(this);
-		if(!success) Log.warning("web","Can't found WebPage to remove it from WebEngineInteraction");
-		setVisibility(false);
-		if(pos!=-1)
-		{
-			if(pos<wi.webPages.size())
-			{
-				wi.setCurPage(wi.webPages.get(pos));
-			}
-		} else
-		{
-			if(wi.webPages.isEmpty()) wi.setCurPage(null);
-			else wi.setCurPage(wi.webPages.lastElement());
+	    if(pos<wi.webPages.size())
+	    {
+		wi.setCurPage(wi.webPages.get(pos));
+	    }
+	} else
+	{
+	    if(wi.webPages.isEmpty()) wi.setCurPage(null); else 
+wi.setCurPage(wi.webPages.lastElement());
+	}
+    }
+
+    @Override public void setVisibility(boolean enable)
+    {
+	if(enable)
+	{ // set visibility for this webpage on and change focus to it later (text page visibility is off)
+	    wi.disablePaint();
+	    Platform.runLater(new Runnable(){@Override public void run() {
+		Log.debug("web","request focus "+webView);
+		webView.setVisible(true);
+		webView.requestFocus();
+	    }});
+	} else
+	{ // set text page visibility to on and current webpage to off
+	    //wi.frame.setVisible(true);
+	    wi.enablePaint();
+	    Platform.runLater(new Runnable(){@Override public void run()
+		    {
+			webView.setVisible(false);
+		    }});
 		}
-	}
-	
-	@Override public void setVisibility(boolean enable)
-	{
-		if(enable)
-		{ // set visibility for this webpage on and change focus to it later (text page visibility is off)
-			wi.disablePaint();
-			Platform.runLater(new Runnable(){@Override public void run()
-			{
-				Log.debug("web","request focus "+webView);
-				webView.setVisible(true);
-				webView.requestFocus();
-			}});
-		} else
-		{ // set text page visibility to on and current webpage to off
-			//wi.frame.setVisible(true);
-			wi.enablePaint();
-			Platform.runLater(new Runnable(){@Override public void run()
-			{
-				webView.setVisible(false);
-			}});
-		}
-	}
-	@Override public boolean getVisibility()
-	{
-		return webView.isVisible();
-	}
-	
-	// rescan current page DOM model and refill list of nodes with it bounded rectangles
-	@Override public void RescanDOM()
-	{
-		Callable<Integer> task=new Callable<Integer>(){
-		@Override public Integer call() throws Exception
+    }
+
+    @Override public boolean getVisibility()
+    {
+	return webView.isVisible();
+    }
+
+    // rescan current page DOM model and refill list of nodes with it bounded rectangles
+    @Override public void RescanDOM()
+    {
+	Callable<Integer> task=new Callable<Integer>(){
+	    @Override public Integer call() throws Exception
+	    {
+		htmlDoc = (HTMLDocument)webEngine.getDocument();
+		htmlWnd =(DOMWindowImpl)((DocumentView)htmlDoc).getDefaultView();
+		dom=new Vector<WebPage.NodeInfo>();
+		domIdx=new LinkedHashMap<org.w3c.dom.Node, Integer>();
+		JSObject js=(JSObject)webEngine.executeScript("(function(){function nodewalk(node){var res=[];if(node){node=node.firstChild;while(node!= null){if(node.nodeType!=3||node.nodeValue.trim()!=='') res[res.length]=node;res=res.concat(nodewalk(node));node=node.nextSibling;}}return res;};var lst=nodewalk(document);var res=[];for(var i=0;i<lst.length;i++){res.push({n:lst[i],r:(lst[i].getBoundingClientRect?lst[i].getBoundingClientRect():(lst[i].parentNode.getBoundingClientRect?lst[i].parentNode.getBoundingClientRect():null))});};return res;})()");;
+		Object o;
+		for(int i=0;!(o=js.getMember(String.valueOf(i))).getClass().equals(String.class);i++)
 		{
-			htmlDoc = (HTMLDocument)webEngine.getDocument();
-			htmlWnd =(DOMWindowImpl)((DocumentView)htmlDoc).getDefaultView();
-			//
-			dom=new Vector<WebPage.NodeInfo>();
-			domIdx=new LinkedHashMap<org.w3c.dom.Node, Integer>();
-			JSObject js=(JSObject)webEngine.executeScript("(function(){function nodewalk(node){var res=[];if(node){node=node.firstChild;while(node!= null){if(node.nodeType!=3||node.nodeValue.trim()!=='') res[res.length]=node;res=res.concat(nodewalk(node));node=node.nextSibling;}}return res;};var lst=nodewalk(document);var res=[];for(var i=0;i<lst.length;i++){res.push({n:lst[i],r:(lst[i].getBoundingClientRect?lst[i].getBoundingClientRect():(lst[i].parentNode.getBoundingClientRect?lst[i].parentNode.getBoundingClientRect():null))});};return res;})()");;
-			Object o;
-			for(int i=0;!(o=js.getMember(String.valueOf(i))).getClass().equals(String.class);i++)
-			{
-				JSObject rect=(JSObject)((JSObject)o).getMember("r");
-				org.w3c.dom.Node n=(org.w3c.dom.Node)((JSObject)o).getMember("n");
-				NodeInfo info=new NodeInfo();
-				if(rect==null)
-				{
-					info.rect=new Rectangle(0,0,0,0);
-				} else
-				{
-					int x=(int)Double.parseDouble(rect.getMember("left").toString());
-					int y=(int)Double.parseDouble(rect.getMember("top").toString());
-					int width=(int)Double.parseDouble(rect.getMember("width").toString());
-					int height=(int)Double.parseDouble(rect.getMember("height").toString());
-					info.rect=new Rectangle(x,y,width,height);
-				}
-				info.node=n;
-				// by default, only leap nodes good for TEXT 
-				info.forTEXT=!n.hasChildNodes();
-				// make decision about TEXT nodes by class
-				if(n instanceof HTMLAnchorElement
-				 ||n instanceof HTMLButtonElement
-				 ||n instanceof HTMLInputElement
-				 //||n.getClass()==com.sun.webkit.dom.HTMLPreElementImpl.class
-				 ||n instanceof HTMLSelectElement
-				 ||n instanceof HTMLTextAreaElement
-				) info.forTEXT=true;
-				//if(info.forTEXT&&info.isVisible()) System.out.println("DOM: node:"+n.getNodeName()+", "+(!(n instanceof HTMLElement)?n.getNodeValue():((HTMLElement)n).getTextContent())); // +" text:"+info.forTEXT+
-				domIdx.put(n, i);
-				dom.add(info);
-				//
-			}
-			// reselect window object (for example if page was reloaded)
-			window=(JSObject)webEngine.executeScript("window");
-			return null;
-		}};
-		FutureTask<Integer> query=new FutureTask<Integer>(task){};
-		if(Platform.isFxApplicationThread()) {try {task.call();} catch(Exception e) {e.printStackTrace();} return;}
-		// call from awt thread 
-		Platform.runLater(query);
-		// waiting for rescan end
-		try {query.get();} catch(InterruptedException|ExecutionException e) {e.printStackTrace();}
-	}
-	
+		    JSObject rect=(JSObject)((JSObject)o).getMember("r");
+		    final org.w3c.dom.Node n=(org.w3c.dom.Node)((JSObject)o).getMember("n");
+		    final NodeInfo info=new NodeInfo();
+		    if(rect==null)
+		    {
+			info.rect=new Rectangle(0,0,0,0);
+		    } else
+		    {
+			int x=(int)Double.parseDouble(rect.getMember("left").toString());
+			int y=(int)Double.parseDouble(rect.getMember("top").toString());
+			int width=(int)Double.parseDouble(rect.getMember("width").toString());
+			int height=(int)Double.parseDouble(rect.getMember("height").toString());
+			info.rect=new Rectangle(x,y,width,height);
+		    }
+		    info.node=n;
+		    // by default, only leaf nodes good for TEXT 
+		    info.forTEXT=!n.hasChildNodes();
+		    // make decision about TEXT nodes by class
+		    if(n instanceof HTMLAnchorElement
+		       ||n instanceof HTMLButtonElement
+		       ||n instanceof HTMLInputElement
+		       //||n.getClass()==com.sun.webkit.dom.HTMLPreElementImpl.class
+		       ||n instanceof HTMLSelectElement
+		       ||n instanceof HTMLTextAreaElement) 
+			info.forTEXT=true;
+		    //if(info.forTEXT&&info.isVisible()) System.out.println("DOM: node:"+n.getNodeName()+", "+(!(n instanceof HTMLElement)?n.getNodeValue():((HTMLElement)n).getTextContent())); // +" text:"+info.forTEXT+
+		    domIdx.put(n, i);
+		    dom.add(info);
+		    //
+		}
+		// reselect window object (for example if page was reloaded)
+		window=(JSObject)webEngine.executeScript("window");
+		return null;
+	    }};
+
+	FutureTask<Integer> query=new FutureTask<Integer>(task){};
+	if(Platform.isFxApplicationThread()) {try {task.call();} catch(Exception e) {e.printStackTrace();} return;}
+	// call from awt thread 
+	Platform.runLater(query);
+	// waiting for rescan end
+	try {
+	    query.get();
+}
+	catch(InterruptedException|ExecutionException e) 
+{
+    e.printStackTrace();
+}
+    }
+
 	// start loading page via link
-	@Override public void load(final String link)
-	{
-		Platform.runLater(new Runnable()
+    @Override public void load(String link)
+    {
+	final String l = link;
+	Platform.runLater(new Runnable() {
+		@Override public void run()
 		{
-			@Override public void run()
-			{
-				webEngine.load(link);
-			}
-		});
-	}
-	// start loading page by it's content
-	@Override public void loadContent(final String text)
-	{
-		Platform.runLater(new Runnable()
+				webEngine.load(l);
+		}});
+    }
+
+    // start loading page by it's content
+    @Override public void loadContent(String text)
+    {
+	final String t = text;
+	Platform.runLater(new Runnable() {
+		@Override public void run()
 		{
-			@Override public void run()
-			{
-				webEngine.loadContent(text);
-			}
-		});
-	}
-	
-	@Override public void stop()
-	{
-		Platform.runLater(new Runnable()
+		    webEngine.loadContent(t);
+		}});
+    }
+
+    @Override public void stop()
+    {
+	Platform.runLater(new Runnable() {
+		@Override public void run()
 		{
-			@Override public void run()
-			{
-				webEngine.getLoadWorker().cancel();
-			}
-		});
-	}
-	
-	@Override public String getTitle()
-	{
-		if(webEngine==null) return null; // FIXME: throw exception when webEngine not ready to use
-		return webEngine.titleProperty().get();
-	}
-	@Override public String getUrl()
-	{
-		if(webEngine==null) return null; // FIXME: throw exception when webEngine not ready to use
-		return webEngine.getLocation();
-	}
-	
-	@Override public Object executeScript(String script)
-	{
-		if(webEngine==null) return null; // FIXME: throw exception when webEngine not ready to use
-		return webEngine.executeScript(script);
-	}
-	
-	@Override public ElementList.SelectorALL selectorALL(boolean visible)
-	{
-		return new WebElementList.SelectorALL(visible);
-	}
-	@Override public ElementList.SelectorTEXT selectorTEXT(boolean visible,String filter)
-	{
-		return new WebElementList.SelectorTEXT(visible,filter);
-	}
-	@Override public ElementList.SelectorTAG selectorTAG(boolean visible,String tagName,String attrName,String attrValue)
-	{
-		return new WebElementList.SelectorTAG(visible,tagName,attrName,attrValue);
-	}
-	@Override public ElementList.SelectorCSS selectorCSS(boolean visible,String tagName,String styleName,String styleValue)
-	{
-		return new WebElementList.SelectorCSS(visible,tagName,styleName,styleValue);
-	}
-	@Override public ElementList elementList()
-	{
-		return new WebElementList(this);
-	}
+		    webEngine.getLoadWorker().cancel();
+		}});
+    }
+
+    @Override public String getTitle()
+    {
+	if(webEngine==null)// return null; // FIXME: throw exception when webEngine not ready to use
+	    throw new NullPointerException("webEngine not initialized");
+	return webEngine.titleProperty().get();
+    }
+
+    @Override public String getUrl()
+    {
+	if(webEngine==null) //return null; // FIXME: throw exception when webEngine not ready to use
+	    throw new NullPointerException("webEngine not initialized");
+	return webEngine.getLocation();
+    }
+
+    @Override public Object executeScript(String script)
+    {
+	if(webEngine==null) //return null; // FIXME: throw exception when webEngine not ready to use
+	    throw new NullPointerException("webEngine initialized");
+	return webEngine.executeScript(script);
+    }
+
+    @Override public ElementList.SelectorALL selectorALL(boolean visible)
+    {
+	return new WebElementList.SelectorALL(visible);
+    }
+
+    @Override public ElementList.SelectorTEXT selectorTEXT(boolean visible,String filter)
+    {
+	return new WebElementList.SelectorTEXT(visible,filter);
+    }
+
+    @Override public ElementList.SelectorTAG selectorTAG(boolean visible,String tagName,String attrName,String attrValue)
+    {
+	return new WebElementList.SelectorTAG(visible,tagName,attrName,attrValue);
+    }
+
+    @Override public ElementList.SelectorCSS selectorCSS(boolean visible,String tagName,String styleName,String styleValue)
+    {
+	return new WebElementList.SelectorCSS(visible,tagName,styleName,styleValue);
+    }
+
+    @Override public ElementList elementList()
+    {
+	return new WebElementList(this);
+    }
 }
