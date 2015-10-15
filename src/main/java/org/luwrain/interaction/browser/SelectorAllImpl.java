cc91c895e1f5ccfeecafeacb3d58028abb52711c
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-10-15 15:18:28 +0600

    Minor selectors cleaning

--- src/main/java/org/luwrain/interaction/browser/SelectorAllImpl.java
+++ src/main/java/org/luwrain/interaction/browser/SelectorAllImpl.java
@@ -6,27 +6,38 @@ import org.luwrain.browser.*;
 // select filter all nodes on page
 
 class SelectorAllImpl extends SelectorImpl implements ElementList.SelectorALL
-	{
-		boolean visible;
-		@Override public boolean isVisible(){	return visible;	}
-		@Override public void setVisible(boolean visible)	{this.visible=visible;}
-		
-SelectorAllImpl(boolean visible)
-		{
-			this.visible=visible;
-		}
-		// return true if current element is visible
-		@Override public boolean checkVisible(ElementList wel_)
-		{
-			ElementListImpl wel=(ElementListImpl)wel_;
-			return wel.current.isVisible();
-		}
-		// return true if current element corresponds this selector
-		@Override public boolean check(ElementList wel_)
-		{
-			ElementListImpl wel=(ElementListImpl)wel_;
-			wel.current=wel.page.dom.get(wel.pos);
-			if(visible&&!checkVisible(wel)) return false;
-			return true;
-		}
-	};
+{
+    boolean visible;
+
+    SelectorAllImpl(boolean visible)
+    {
+	this.visible=visible;
+    }
+
+    @Override public boolean isVisible()
+    {
+	return visible;	
+    }
+
+    @Override public void setVisible(boolean visible)	
+    {
+	this.visible=visible;
+    }
+
+    // return true if current element is visible
+    @Override public boolean checkVisible(ElementList wel_)
+    {
+	ElementListImpl wel=(ElementListImpl)wel_;
+	return wel.current.isVisible();
+    }
+
+    // return true if current element corresponds this selector
+    @Override public boolean check(ElementList wel_)
+    {
+	ElementListImpl wel=(ElementListImpl)wel_;
+	wel.current=wel.page.dom.get(wel.pos);
+	if(visible&&!checkVisible(wel))
+	    return false;
+	return true;
+    }
+}
--- src/main/java/org/luwrain/interaction/browser/SelectorImpl.java
+++ src/main/java/org/luwrain/interaction/browser/SelectorImpl.java
@@ -6,11 +6,13 @@ import org.luwrain.browser.*;
 abstract class SelectorImpl implements ElementList.Selector
 {
     @Override public abstract boolean check(ElementList wel);
+
     @Override public boolean first(ElementList wel_)
     {
-	ElementListImpl wel=(ElementListImpl)wel_;
+	final ElementListImpl wel=(ElementListImpl)wel_;
 	wel.pos=0;
-	while(wel.pos<wel.page.domIdx.size()&&!check(wel)) wel.pos++;
+	while(wel.pos<wel.page.domIdx.size()&&!check(wel)) 
+	    ++wel.pos;
 	if(wel.pos>=wel.page.domIdx.size())
 	{
 	    wel.pos=0;
@@ -23,8 +25,9 @@ abstract class SelectorImpl implements ElementList.Selector
     {
 	ElementListImpl wel=(ElementListImpl)wel_;
 	int prev=wel.pos;
-	wel.pos++;
-	while(wel.pos<wel.page.domIdx.size()&&!check(wel)) wel.pos++;
+	++wel.pos;
+	while(wel.pos<wel.page.domIdx.size()&&!check(wel)) 
+	    ++wel.pos;
 	if(wel.pos>=wel.page.domIdx.size())
 	{
 	    wel.pos=prev;
@@ -35,7 +38,7 @@ abstract class SelectorImpl implements ElementList.Selector
 
     @Override public boolean prev(ElementList wel_)
     {
-	ElementListImpl wel=(ElementListImpl)wel_;
+	final ElementListImpl wel=(ElementListImpl)wel_;
 	int prev=wel.pos;
 	wel.pos--;
 	while(wel.pos>0&&!check(wel)) wel.pos--;
@@ -50,15 +53,19 @@ abstract class SelectorImpl implements ElementList.Selector
     @Override public boolean to(ElementList wel,int pos)
     {
 	if(wel.getPos()==pos)
-	    return true;
-	else if(wel.getPos()<pos)
-	{ // need next
-	    while(next(wel)) if(pos==wel.getPos()) return true;
-	    return false;
-	} else
-	{ // need prev
-	    while(prev(wel)) if(pos==wel.getPos()) return true;
-	    return false;
-	}
+	    return true; else 
+	    if(wel.getPos()<pos)
+	    { // need next
+		while(next(wel)) 
+		    if(pos==wel.getPos()) 
+			return true;
+		return false;
+	    } else
+	    { // need prev
+		while(prev(wel)) 
+		    if(pos==wel.getPos()) 
+			return true;
+		return false;
+	    }
     }
 }
