d35497ff38be9b72c5f2e5780b182fa3feb8cec6
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-02-09 08:52:51 +0600

    Copyright date update

--- src/main/java/org/luwrain/interaction/javafx/SelectorImpl.java
+++ src/main/java/org/luwrain/interaction/javafx/SelectorImpl.java
@@ -1,6 +1,6 @@
 /*
-   Copyright 2015 Roman Volovodov <gr.rPman@gmail.com>
-   Copyright 2012-2015 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2015-2016 Roman Volovodov <gr.rPman@gmail.com>
+   Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
 
    This file is part of the LUWRAIN.
 
